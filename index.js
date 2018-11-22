e54d37d3d49abbf946d205a192f33860ce8cfc21
Andrew Avdeev <andrewww.avdeev@gmail.com>
2018-11-22 13:11:45 +0300

    Upgrade schema and some styles

--- index.js
+++ index.js
@@ -44,3 +44,30 @@ Handlebars.registerHelper("formatDate", function(datetime) {
 Handlebars.registerHelper("httpReplacer", function(url) {
   return url.replace(/(^\w+:|^)\/\//, '');
 });
+
+Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+	switch (operator) {
+			case '==':
+					return (v1 == v2) ? options.fn(this) : options.inverse(this);
+			case '===':
+					return (v1 === v2) ? options.fn(this) : options.inverse(this);
+			case '!=':
+					return (v1 != v2) ? options.fn(this) : options.inverse(this);
+			case '!==':
+					return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+			case '<':
+					return (v1 < v2) ? options.fn(this) : options.inverse(this);
+			case '<=':
+					return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+			case '>':
+					return (v1 > v2) ? options.fn(this) : options.inverse(this);
+			case '>=':
+					return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+			case '&&':
+					return (v1 && v2) ? options.fn(this) : options.inverse(this);
+			case '||':
+					return (v1 || v2) ? options.fn(this) : options.inverse(this);
+			default:
+					return options.inverse(this);
+	}
+});
